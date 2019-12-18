f3bf5049c01a32d1479f40918f68f90348313173
Mina <parkmina9644@gmail.com>
2019-12-18 17:02:05 +0900

    ButtonTable make

--- Main1218_1700.py
+++ Main1218_1700.py
@@ -13,16 +13,7 @@ import time
 
 
 # 버튼 기능 설정
-class Button(QToolButton):
-    def __init__(self, image, callback):
-        super().__init__()
-        self.clicked.connect(callback)
 
-    def sizeHint(self):
-        size = super(Button, self).sizeHint()
-        size.setHeight(size.height() + 20)
-        size.setWidth(max(size.width(), size.height()))
-        return size
 
 
 class GhostWarrior(QWidget):
